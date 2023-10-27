74aae2951db55bb6eb3ddfb0d4e320bcfb981759
antonny melo <antonny.mello@gmail.com>
2023-10-27 17:49:18 -0300

    chore: config env file

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,10 @@
 import { knex as Knex, Knex as KnexConfig } from 'knex';
 
+if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not defined');
+
 export const config: KnexConfig.Config = {
   client: 'sqlite',
-  connection: { filename: './database/app.db' },
+  connection: { filename: process.env.DATABASE_URL },
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
