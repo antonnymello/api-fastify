4dcffca9c1da592783a54d44c66200179d49b039
antonny melo <antonny.mello@gmail.com>
2023-11-11 16:12:55 -0300

    test: list transactions route

--- tests/transactions.spec.ts
+++ tests/transactions.spec.ts
@@ -3,6 +3,16 @@ import request from 'supertest';
 
 import { app } from '../src/app';
 
+interface CreateTransactionBody {
+  title: string;
+  amount: number;
+  type: 'credit' | 'debit';
+}
+
+const createTransaction = async (requestBody: CreateTransactionBody) => {
+  return request(app.server).post('/transactions').send(requestBody);
+};
+
 describe('Transactions routes', () => {
   beforeAll(async () => {
     await app.ready();
@@ -12,13 +22,31 @@ describe('Transactions routes', () => {
     await app.close();
   });
 
-  it('should create a new transaction', async () => {
-    const response = await request(app.server).post('/transactions').send({
+  it('should be able to create a new transaction', async () => {
+    const response = await createTransaction({
       title: 'NEW_TRANSACTION',
       amount: 1000,
       type: 'credit',
     });
 
     expect(response.statusCode).toBe(201);
+  });
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await createTransaction({
+      title: 'NEW_TRANSACTION',
+      amount: 1000,
+      type: 'credit',
+    });
+
+    const cookies = createTransactionResponse.get('set-cookie');
+
+    const listTransactionResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies);
+
+    expect(listTransactionResponse.body.transactions).toEqual([
+      expect.objectContaining({ title: 'NEW_TRANSACTION', amount: 1000 }),
+    ]);
   });
 });
